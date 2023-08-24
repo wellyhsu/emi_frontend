26aaf8448daa44a17ba25369ff90db50af69f932
Ywellyhsu <welly9166@gmail.com>
2023-08-24 15:35:47 +0000

    update(8/24)new

--- pages/api/video.ts
+++ pages/api/video.ts
@@ -1,7 +1,7 @@
 // pages/api/video.js
 import fs from 'fs';
 import path from 'path';
-
+/*
 export default function handler(req, res) {
 //  const videoPath = process.env.NEXT_PUBLIC_video_path;
   const videoPath = req.body;
@@ -12,3 +12,26 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'video/mp4');
   videoStream.pipe(res);
 }
+*/
+async function fetchData() {
+  try {
+    const response = await fetch('your-backend-api-url');
+    const data = await response.json();
+    console.log('Data from backend:', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+// 在适当的地方调用 fetchData 函数
+async function yourFunction() {
+  try {
+    const data = await fetchData();
+    // 在这里使用获取到的数据
+  } catch (error) {
+    // 处理错误
+  }
+}
+
